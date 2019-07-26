2463c2d6f7244aab3eac193f5bdc8f656fedaa56
Mason Trimbell <mtmaster3@gmail.com>
2019-07-26 13:15:54 -0500

    Finish monster classes and add appliance classes

---
+++ src/CoffeeMaker.kt
@@ -0,0 +1,8 @@
+class CoffeeMaker: Appliance() {
+    override val color = ""
+    var coffeeLeft = false
+
+    override fun consumePower() = println("Consuming power")
+    fun fillWithWater() = println("Fill with water")
+    fun makeCoffee() = println("Make the coffee")
+}
